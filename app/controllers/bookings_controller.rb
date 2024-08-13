ac45895b34f3768288b1c44c978ae37027bb7db9
Auriane Durand <aurianed@hotmail.fr>
2024-08-13 14:30:14 +0200

    init bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,18 @@
 class BookingsController < ApplicationController
+
+  def new
+    @offer = Offer.find(params[:offer_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @offer = Offer.find(params[:offer_id])
+    @booking.offer = @booking
+    if @booking.save
+      redirect_to booking_my_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
