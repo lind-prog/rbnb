a24c4f6f158bf112649b61b179c498ef87fc3afe
Linda Badji <linda.badji33@gmail.com>
2024-08-13 15:48:37 +0200

    offer controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,20 @@
 class OffersController < ApplicationController
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    if @offer.save
+      redirect_to @offer, notice: "L'offre a été créée avec succés !"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :description, :price)
+  end
 end
