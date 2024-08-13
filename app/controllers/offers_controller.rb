f68358fbed17c6af05f2f38d70944017c492ba1b
Adrien Astoux <adrien.astoux@gmail.com>
2024-08-13 14:30:13 +0200

    offers show et index

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,6 +1,5 @@
 class OffersController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_offer, only: [:show]
+  before_action :set_offer, only: :show
 
   def index
     @offers = Offer.all
@@ -9,28 +8,9 @@ class OffersController < ApplicationController
   def show
   end
 
-  def new
-    @offer = Offer.new
-  end
-
-  def create
-    @offer = current_user.offers.build(offer_params)
-    if @offer.save
-      redirect_to @offer, notice: 'Offer created successfully!'
-    else
-      render :new
-    end
-  end
-
   private
 
   def set_offer
     @offer = Offer.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    redirect_to offers_path, alert: 'Offer not found.'
-  end
-
-  def offer_params
-    params.require(:offer).permit(:title, :description, :price)
   end
 end
