74ae17748f00c3bebfb17790a9553412eace1b67
Linda Badji <linda.badji33@gmail.com>
2024-08-14 16:03:55 +0200

    create new

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,5 +2,5 @@ class Offer < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_one_attached :image
-  validates :title, :number_of_fan, :activity, :price, :image, presence: true
+  validates :title, :number_of_fan, :activity, :price, presence: true
 end
