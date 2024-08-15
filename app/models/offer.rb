552e51ec8e019d4f0c9893b85afd172a9fc002a0
Adrien Astoux <adrien.astoux@gmail.com>
2024-08-15 15:15:10 +0200

    barre recherche et co

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,12 +1,14 @@
-# app/models/offer.rb
 class Offer < ApplicationRecord
   include PgSearch::Model
+
+  belongs_to :user
+  has_many :bookings
+  has_one_attached :image
+  validates :title, :number_of_fan, :activity, :price, presence: true
 
   pg_search_scope :global_search,
     against: [ :title, :activity ],
     using: {
       tsearch: { prefix: true } # Permet la recherche partielle, ex: "super" matchera avec "superman"
     }
-
-  # Autres associations et validations
 end
